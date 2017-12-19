25873704ec79641a270b1cfca2cec848db5550c9
Caitlin Barron <barroncn@gmail.com>
2017-12-19 22:36:40 +0000

    Sequelize Changes Commented

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,9 +8,8 @@ var express = require("express"); // require the express module
 var router = express.Router();
 
 router.get("/", function(req,res){
+    //When a get request is sent to "/", Sequelize's findAll function is called to get all the entries in the pasta table
     db.pasta.findAll({}).then(function(data){
-       console.log("data");
-       console.log(data);
        var hbsObj = {pasta: data}; // data is our array of data objects for each pasta, put in an object so we can pass it to handlebars
        res.render("index", hbsObj); // We want to render the index page with the pasta objects in it
    }); 
@@ -18,6 +17,7 @@ router.get("/", function(req,res){
 
 router.post("/", function(req,res){
     if(req.body.newNoodle != ""){//as long as the user entered something into input
+    //When a post request is sent to "/", Sequelize's create function is called to insert a new pasta row into the pasta table
         db.pasta.create({pasta_name: req.body.newNoodle}).then(function(data){//the new pasta name is gotten from the "name=newNoodle" in the POST form
             //if(err) {return res.status(500).end();}
             res.redirect("/"); //redirect to get "/" to reload page with new entry
@@ -26,6 +26,7 @@ router.post("/", function(req,res){
 });
 
 router.put("/:id", function(req,res){
+    //When a put request is sent to "/id", Sequelize's update function is called to update devoured to true in the row twith the given id
     db.pasta.update({devoured: true}, {where:{id:req.params.id}}).then(function(data){//the id is gotten from the parameters
         //if(err) {return res.status(500).end();}
         res.status(200).end();//status = good to go! (the page reload will take place in clientside js)
--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
+  //Create a new table called "pasta" with pasta_name and devoured columns.
+  //Sequelize will automatically create timestamp and id columns.
   var Pasta = sequelize.define("pasta", {
     pasta_name: {
                     type: DataTypes.STRING,
-                    allowNull: false,
+                    allowNull: false, //A pasta_name must be entered
                     validate: {
-                                len: [1]
+                                len: [1] //The length must be at least 1
                               }
                 },
     devoured: {
                 type: DataTypes.BOOLEAN,
-                defaultValue: false
+                defaultValue: false //Devoured will default to false
               }
     });
-  return Pasta;
+  return Pasta; //Return the Pasta object to it can be used
 };
